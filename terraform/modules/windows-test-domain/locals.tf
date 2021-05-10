e31223eded37d81252ce476495f7f4f543588a3e
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-10 15:54:51 +0100

    Add env vars and update install script to use S3
    Not tested yet.
    This requires the splunk-apps role to have been created in test.
    Once that's done I need to update the trusts
    Then I need to update the exec-role.

--- terraform/modules/windows-test-domain/locals.tf
+++ terraform/modules/windows-test-domain/locals.tf
@@ -7,22 +7,24 @@ locals {
     SvcCodeURL    = "https://github.com/alphagov/cyber-security-terraform"
   }
 
-  env_variables   = {
-    "environment": var.environment
+  env_variables = {
+    "environment" : var.environment,
+    "splunk_forwarder_name" : var.splunk_forwarder_name,
+    "splunk_config_bucket" : var.splunk_config_bucket,
+    "s3_host_account_id" : data.aws_caller_identity.current.account_id
   }
 
-
-  user_data_ps1    = file("${path.module}/scripts/WinRM/user_data.ps1")
-  user_data        = <<END
+  user_data_ps1 = file("${path.module}/scripts/WinRM/user_data.ps1")
+  user_data     = <<END
 <powershell>
 ${local.user_data_ps1}
 
 # add env vars
 $profile_set_env_vars = @"
 # setting local environment variables
-%{ for var_name, value in local.env_variables }
+%{for var_name, value in local.env_variables}
 `$env:${var_name} = `"${value}`"
-%{ endfor }
+%{endfor}
 "@
 
 Try {
