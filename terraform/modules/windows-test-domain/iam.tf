9ff223c024785bd76c6b137cec9c9c8f8a0eeaaa
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-11 09:21:36 +0100

    Add STS AssumeRole permission to S3 SplunkForwarderRoles

--- terraform/modules/windows-test-domain/iam.tf
+++ terraform/modules/windows-test-domain/iam.tf
@@ -60,7 +60,7 @@ data "aws_iam_policy_document" "wec_exec_role_policy" {
     ]
 
     resources = [
-
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/SplunkForwarderRole*"
     ]
   }
 }
