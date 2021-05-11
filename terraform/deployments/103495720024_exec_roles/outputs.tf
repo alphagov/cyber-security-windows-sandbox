06af911d7805818bbe63bc82a869273981249c67
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-11 13:51:39 +0100

    Fix outputs and vars for exec role terraform

--- terraform/deployments/103495720024_exec_roles/outputs.tf
+++ terraform/deployments/103495720024_exec_roles/outputs.tf
@@ -1,14 +1,4 @@
-output "dc_public_ip" {
-  description = "The ID of the VPC"
-  value       = module.windows_test_domain_module.dc_public_ip
-}
-
-output "wec_public_ip" {
-  description = "The ID of the VPC"
-  value       = module.windows_test_domain_module.wec_public_ip
-}
-
-output "user_data" {
-  description = "The script run on startup"
-  value       = module.windows_test_domain_module.user_data
+output "wec_exec_role_arn" {
+  description = "The ARN of the WEC instance profile role"
+  value       = aws_iam_role.wec_exec_role.arn
 }
