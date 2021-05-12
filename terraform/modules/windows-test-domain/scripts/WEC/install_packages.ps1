f6c7863a8e21481603daa40237ab4b4223e25426
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-12 08:50:09 +0100

    Pass metadata hash to check hash function

--- terraform/modules/windows-test-domain/scripts/WEC/install_packages.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/install_packages.ps1
@@ -89,7 +89,7 @@ $METADATA_CSV=(Import-CSV $PSScriptRoot/packages/metadata.csv)
 ForEach ($package in $METADATA_CSV){
   $file = $package."file"
   $hash = $package."hash"
-  $valid=(Check-HashIsValid -bucket $BUCKET_NAME -path packages -file $file -creds $ROLE_SESSION)
+  $valid=(Check-HashIsValid -bucket $BUCKET_NAME -path packages -file $file -hash $hash -creds $ROLE_SESSION)
   Write-Host "Hash is valid: $valid"
 
   If ($valid -eq 1)
