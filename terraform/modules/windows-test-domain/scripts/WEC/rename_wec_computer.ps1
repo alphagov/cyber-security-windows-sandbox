21ca0c26ec6f0273ba1d662b623bd9f2364c023d
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-13 14:43:25 +0100

    Change computer names - specify localhost

--- terraform/modules/windows-test-domain/scripts/WEC/rename_wec_computer.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/rename_wec_computer.ps1
@@ -4,4 +4,4 @@
 #Resources -
 #Microsoft-Docs:https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-6
 #Microsoft-Docs: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-credential?view=powershell-6
-Rename-computer -NewName "WSWEC01"
+Rename-computer -ComputerName localhost -NewName "WSWEC01"
