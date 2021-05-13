6a45a912794f7ebccf0ee0e22ecdbe6f2c0437cf
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-13 19:28:35 +0100

    Add unsecure option to domain join

--- terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
@@ -1,1 +1,1 @@
-Add-Computer -DomainName "$env:domain" -Server SHIRE\WSDC01 -OUPath "OU=Servers,DC=shire,DC=com"
+Add-Computer -DomainName "$env:domain" -Server SHIRE\WSDC01 -OUPath "OU=Servers,DC=shire,DC=com" -Unsecure
