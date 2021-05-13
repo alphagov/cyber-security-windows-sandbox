abaaeea3e21a9bf121dd201d70086ea597dff053
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-13 18:44:45 +0100

    Remove forced restart from join_domain

--- terraform/modules/windows-test-domain/ec2-wec.tf
+++ terraform/modules/windows-test-domain/ec2-wec.tf
@@ -38,10 +38,10 @@ resource "aws_instance" "wec" {
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WinRM\\templating.ps1",
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\registry_system_enableula_sacl.ps1",
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\registry_terminal_server_sacl.ps1",
-      "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\install_packages.ps1",
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\rename_wec_computer.ps1",
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\join_domain.ps1",
       "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\configure_wec.ps1",
+      "powershell C:\\alphagov-windows-sandbox\\terraform\\modules\\windows-test-domain\\scripts\\WEC\\install_packages.ps1",
       "powershell C:\\Set-AuditRule\\Set-AuditRule.ps1",
       "powershell Restart-Computer -Force",
     ]
--- terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
@@ -1,3 +1,1 @@
-Add-Computer -DomainName "$env:domain" -OUPath "OU=Servers,DC=shire,DC=com"
-
-Restart-Computer -Force
+Add-Computer -DomainName "$env:domain" -Server SHIRE\WSDC01 -OUPath "OU=Servers,DC=shire,DC=com"
