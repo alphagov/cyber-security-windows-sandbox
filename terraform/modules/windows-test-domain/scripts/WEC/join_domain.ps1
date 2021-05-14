de1045c969fd508e8bda396b729ec85b039fa52e
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-14 14:58:51 +0100

    Use NetBIOS domain in join domain script.

--- terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/join_domain.ps1
@@ -1,6 +1,6 @@
-$User = "$env:domain\wecserver"
+$User = "SHIRE\wecserver"
 $Password = ConvertTo-SecureString -String "$env:DOMAIN_PASSWORD" -AsPlainText -Force # pragma: allowlist secret
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Password
 
 Add-Computer -WorkGroupName Servers
-Add-Computer -DomainName "$env:domain" -Server $env:DOMAIN_CONTROLLER_IP -OUPath "OU=Servers,DC=shire,DC=com" -Unsecure -Credential $Credential -Force -Options "JoinWithNewName,AccountCreate"
+Add-Computer -DomainName "SHIRE" -Server $env:DOMAIN_CONTROLLER_IP -OUPath "OU=Servers,DC=shire,DC=com" -Unsecure -Credential $Credential -Force -Options "JoinWithNewName,AccountCreate"
