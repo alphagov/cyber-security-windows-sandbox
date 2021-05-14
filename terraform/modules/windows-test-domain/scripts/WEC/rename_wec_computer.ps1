49c2e185999b6327376a156dcce7037f7bf2725c
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-14 11:16:18 +0100

    Domain suffix can't be set in hostname.

--- terraform/modules/windows-test-domain/scripts/WEC/rename_wec_computer.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/rename_wec_computer.ps1
@@ -4,4 +4,4 @@
 #Resources -
 #Microsoft-Docs:https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-6
 #Microsoft-Docs: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-credential?view=powershell-6
-Rename-computer -ComputerName localhost -NewName "WSWEC01.$env:DOMAIN"
+Rename-computer -ComputerName localhost -NewName "WSWEC01"
