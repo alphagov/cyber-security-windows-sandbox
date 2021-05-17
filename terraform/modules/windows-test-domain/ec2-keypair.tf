5c0a3a00e6f6aa3873d5a0ab0b9efe53d4994e2c
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-05-17 17:17:37 +0100

    Leave the keypair in place and create a new version
    It turns out deleting secrets from secrets-manager is not easy.
    You have to schedule them for deletion and then you can't recreate
    that secret until it has been deleted.
    
    My suggestion is we add the secret to the exec_roles terraform
    and import the existing secret.

--- terraform/modules/windows-test-domain/ec2-keypair.tf
+++ terraform/modules/windows-test-domain/ec2-keypair.tf
@@ -1,9 +1,9 @@
-resource "aws_secretsmanager_secret" "ssh_keypair" {
-  name       = var.public_key_name
+data "aws_secretsmanager_secret" "ssh_keypair" {
+  name = var.public_key_name
 }
 
 resource "aws_secretsmanager_secret_version" "ssh_keypair" {
-  secret_id     = aws_secretsmanager_secret.ssh_keypair.id
+  secret_id     = data.aws_secretsmanager_secret.ssh_keypair.id
   secret_string = jsonencode({
     private = file(var.private_key_path)
     public  = file(var.public_key_path)
