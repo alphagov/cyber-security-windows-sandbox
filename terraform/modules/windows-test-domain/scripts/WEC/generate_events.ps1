6e8117cf2413419ae9e563a10dd545946771bc53
alice-carr <alice.bc.carr@gmail.com>
2021-05-26 16:28:56 +0100

    create system event

--- terraform/modules/windows-test-domain/scripts/WEC/generate_events.ps1
+++ terraform/modules/windows-test-domain/scripts/WEC/generate_events.ps1
@@ -1,6 +1,13 @@
 # Set up new source for application log
-New-EventLog -Source TestSource -LogName Application
+New-EventLog -Source WECTestApplicationSource -LogName Application
 # Send a test event through
-Write-EventLog -LogName "Application" -Source TestSource -EventID 3001 -EntryType Information -Message "This is a test message"
+Write-EventLog -LogName "Application" -Source WECTestApplicationSource   -EventID 3001 -EntryType Information -Message "This is a test message"
 # Check test event has been sent
 Get-WinEvent -FilterHashtable @{logname='Application'; ID = 3001} | Where-Object  { $_.Message -Match "This is a test message"}
+
+# Set up new source for system log
+New-EventLog -Source WECTestSystemSource -LogName System
+# Send a test event through
+Write-EventLog -LogName "System" -Source WECTestSystemSource -EventID 3001 -EntryType Information -Message "This is a test message"
+# Check test event has been sent
+Get-WinEvent -FilterHashtable @{logname='System'; ID = 3001} | Where-Object  { $_.Message -Match "This is a test message"}
